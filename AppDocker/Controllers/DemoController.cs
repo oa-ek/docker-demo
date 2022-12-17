b09a56ff5eb8653d75ab6e1b9c1fac0144937d6e
Yurii Kleban <yurakleban@gmail.com>
2022-12-17 08:33:00 +0200

    fix

---
+++ AppDocker/Controllers/DemoController.cs
@@ -0,0 +1,26 @@
+﻿using AppDocker.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppDocker.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DemoController : ControllerBase
+    {
+        private readonly DataContext ctx;
+
+        public DemoController(DataContext ctx)
+        {
+            this.ctx = ctx;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<DemoData>> Get()
+        {
+            await ctx.Demos.AddAsync(new DemoData());
+            return await ctx.Demos.ToListAsync();
+        }
+    }
+}
---
+++ AppDocker/Data/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace AppDocker.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<DemoData> Demos { get; set; }
+
+        
+    }
+
+    public class DemoData
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public DateTime CreatedOn { get; set; } = DateTime.Now;
+    }
+}
